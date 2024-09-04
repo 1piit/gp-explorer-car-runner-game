baaa6fbc7cad381a9072433d42e5eab332a86b7c
Pierre Bride <86304014+1piit@users.noreply.github.com>
2024-09-04 13:49:44 +0200

    chore: add some behaviors to learn pixi.js

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ import {
   Sprite,
   Text,
   TextStyle,
+  TilingSprite,
 } from 'pixi.js';
 import { initDevtools } from '@pixi/devtools';
 
@@ -20,18 +21,29 @@ import { initDevtools } from '@pixi/devtools';
   app.canvas.style.position = 'absolute';
   document.body.appendChild(app.canvas) as HTMLCanvasElement;
 
-  const rectangle = new Graphics()
-    .rect(200, 200, 200, 180)
-    .fill({
-      color: 0xffea00,
-      alpha: 0.5,
-    })
-    .stroke({
-      width: 8,
-      color: 0x00ff00,
-    });
+  //   const rectangle = new Graphics()
+  //     .rect(200, 200, 200, 180)
+  //     .fill({
+  //       color: 0xffea00,
+  //       alpha: 0.5,
+  //     })
+  //     .stroke({
+  //       width: 8,
+  //       color: 0x00ff00,
+  //     });
+  //   app.stage.addChild(rectangle);
 
-  app.stage.addChild(rectangle);
+  const textureRoute = await Assets.load('images/route.png');
+  const bgSprite = new TilingSprite({
+    texture: textureRoute,
+    width: app.screen.width,
+    height: app.screen.height,
+  });
+  app.stage.addChild(bgSprite);
+  //   bgSprite.tileScale.set(1, 1);
+  app.ticker.add(function () {
+    bgSprite.tilePosition.y += 2;
+  });
 
   const style = new TextStyle({
     align: 'center',
@@ -47,22 +59,18 @@ import { initDevtools } from '@pixi/devtools';
       distance: 6,
     },
   });
-
   const text = new Text({
     text: 'GP Explorer - Car Runner Game',
     style,
   });
   text.position.set(100, 20);
-
   app.stage.addChild(text);
 
-  const texture = await Assets.load('images/formule-4.png');
-
-  const sprite = new Sprite(texture);
+  const textureCar = await Assets.load('images/formule-4.png');
+  const sprite = new Sprite(textureCar);
   sprite._position.set(300, 300);
   sprite.scale.set(0.5);
   sprite.anchor.set(0.5, 0.5);
-  sprite.rotation = 0.6;
-
+  sprite.rotation = 0;
   app.stage.addChild(sprite);
 })();
